460565424e962f6f850b4044f1b293845981d8a8
Sergey Pichugin <pichuginsergey@hotmail.com>
2015-12-14 09:36:27 +0300

    11

---
+++ DevTube/DevTube.Business/DatabaseOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevTube.Data;
+
+namespace DevTube.Business
+{
+    public static class DatabaseOperations
+    {
+        public static object GetStats()
+        {
+            return new MongoDbContext().Database.GetCollection<string>("col1")
+            ;
+        }
+    }
+}
---
+++ DevTube/DevTube.Data/MongoDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevTube.Data.Properties;
+using MongoDB.Driver;
+
+namespace DevTube.Data
+{
+    public class MongoDbContext
+    {
+        public IMongoDatabase Database { get; private set; }
+
+        public IMongoClient Client { get; private set; }
+
+        public MongoDbContext()
+        {
+            Client = new MongoClient(Settings.Default.DevTubeDatabaseConnectionString);
+            Database = Client.GetDatabase(Settings.Default.DevTubeDatabaseName);
+            }
+        
+    }
+}
